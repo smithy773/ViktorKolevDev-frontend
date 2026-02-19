5ad304d2ac360428a7d9dfb1962af8f0acc21028
Viktor Kolev <viktorkolev0707@gmail.com>
2026-02-19 10:59:02 +0200

    Update theme changes to reflect in markdown

--- src/components/Helpers/MarkdownRenderer/MarkdownRenderer.tsx
+++ src/components/Helpers/MarkdownRenderer/MarkdownRenderer.tsx
@@ -4,11 +4,15 @@ import remarkGfm from "remark-gfm";
 //@ts-ignore
 import "github-markdown-css";
 import "./MarkdownRenderer.css";
+import rehypeHighlight from "rehype-highlight";
 
 function MarkdownRenderer({ children }: any) {
   return (
     <div className="markdown-body">
-      <Markdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw, rehypeHighlight]}
+      >
         {children}
       </Markdown>
     </div>
