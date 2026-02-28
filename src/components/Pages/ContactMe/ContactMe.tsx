1a53488de558fed78230f9fac22502016e518125
Viktor Kolev <viktorkolev0707@gmail.com>
2026-02-28 15:06:53 +0200

    Fix ContactMe styles, add emailjs functionality

--- src/components/Pages/ContactMe/ContactMe.tsx
+++ src/components/Pages/ContactMe/ContactMe.tsx
@@ -1,6 +1,24 @@
 import PageCont from "../../Helpers/PageCont/PageCont";
+import emailjs from "@emailjs/browser";
+
+emailjs.init({ publicKey: "7Hmud7QnYWYG4CcBD" });
 
 function ContactMe() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // @ts-ignore
+    emailjs
+      .sendForm("service_g2e1bkt", "template_g5lkrax", e.currentTarget)
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        },
+      );
+  };
   return (
     <PageCont
       title="Contact me"
@@ -9,10 +27,11 @@ function ContactMe() {
       <div className="flex justify-center w-full">
         <form
           id="contact-form"
-          className="flex flex-col bg-item-background p-6 w-full mx-25"
+          className="flex flex-col bg-item-background p-6 w-full mx-25 rounded-lg"
+          onSubmit={(e) => handleSubmit(e)}
         >
           <input type="hidden" name="time" value="Mar 10 2025 08:46" />
-          <section className="flex gap-5 justify-between">
+          <section className="grid grid-cols-2 grid-rows-6 gap-4">
             <div className="contact-label-input-div">
               <label className="contact-label">Name</label>
               <input
@@ -25,29 +44,35 @@ function ContactMe() {
             <div className="contact-label-input-div">
               <label className="contact-label">Email</label>
               <input
-                className="contact-input"
+                className="contact-input text-lg p-1.5 w-full"
                 type="email"
                 name="email"
                 required
               />
             </div>
+            <div className="contact-label-input-div col-span-2">
+              <label className="contact-label">Subject</label>
+              <input
+                className="contact-input"
+                type="text"
+                name="title"
+                required
+              />
+            </div>
+            <div className="contact-label-input-div col-span-2 row-span-4">
+              <label className="contact-label">Message</label>
+              <textarea
+                className="contact-input h-full"
+                name="message"
+                required
+              ></textarea>
+            </div>
           </section>
-          <div className="contact-label-input-div">
-            <label className="contact-label">Subject</label>
-            <input
-              className="contact-input"
-              type="text"
-              name="title"
-              required
-            />
-          </div>
-          <label className="contact-label">Message</label>
-          <textarea
-            className="contact-input"
-            name="message"
-            required
-          ></textarea>
-          <input type="submit" value="Send" />
+          <input
+            type="submit"
+            value="Send"
+            className="formSubmitBtn mt-6 border font-semibold py-2 px-4 rounded cursor-pointer transition-all"
+          />
         </form>
       </div>
     </PageCont>
