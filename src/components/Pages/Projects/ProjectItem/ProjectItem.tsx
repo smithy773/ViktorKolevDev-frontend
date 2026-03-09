d58e1271dc610accf4d7c06e927ba0d4be0b8a34
Viktor Kolev <viktorkolev0707@gmail.com>
2026-03-09 10:31:19 +0200

    Add responsive styling to Projects page

--- src/components/Pages/Projects/ProjectItem/ProjectItem.tsx
+++ src/components/Pages/Projects/ProjectItem/ProjectItem.tsx
@@ -7,7 +7,7 @@ type ProjectItemProps = {
 
 function ProjectItem({ title, description, gitHubURL, url }: ProjectItemProps) {
   return (
-    <div className="shadow_under bg-item-background px-5 py-1.5 border border-lines rounded-md gap-1.5 w-67 h-64 flex flex-col justify-around">
+    <div className="shadow_under bg-item-background px-5 py-1.5 border border-lines rounded-md gap-1.5 w-67 h-64 flex flex-col justify-around project-item-container">
       <h3 className="text-2xl font-semibold text-highlight py-2">{title}</h3>
       <p className="pl-3 pr-2 w-fill max-w-lg h-30 text-wrap text-md">
         {description}
--- src/components/Pages/Projects/Projects.tsx
+++ src/components/Pages/Projects/Projects.tsx
@@ -8,7 +8,7 @@ function Projects() {
       title="Projects"
       description="Here you can find all of my projects! I try to work on something different every time, so you can find games, web apps, desktop apps and more! Each project has a GitHub repository up on my GitHub account, so feel free to check it out and follow me there for more projects in the future!"
     >
-      <section className="grid grid-cols-3 gap-3">
+      <section className="grid project-container  justify-items-center">
         {projects.length > 0
           ? projects.map((item) => (
               <ProjectItem
